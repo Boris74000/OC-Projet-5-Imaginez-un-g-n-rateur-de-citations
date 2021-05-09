41e4de830201dab7b7a08b0e7f4b2ead9563c961
Charles-Boris DAUVIER <dauvierboris@hotmail.com>
2021-05-09 16:24:45 +0200

    Style global

--- js/Generate.js
+++ js/Generate.js
@@ -2,9 +2,11 @@ class Generate {
     constructor(generateChurchill, generateEinstein) {
         this.generateChurchill = generateChurchill;
         this.generateEinstein = generateEinstein;
+        // this.authorValue = document.getElementById("quoteAuthorSelect").value;
         this.events()
     }
 
+    // Méthode pour récupérer la valeur de l'auteur
     getQuoteAuthor() {
         this.authorValue = document.getElementById("quoteAuthorSelect").value;
 
@@ -15,7 +17,7 @@ class Generate {
         }
     }
 
-
+    // Méthode pour récupérer le nombre de citations choisi
     getQuoteNumber() {
         this.quoteNumber = document.getElementById("quoteNumberSelect").value;
         if (this.quoteNumber === "empty") {
@@ -25,58 +27,54 @@ class Generate {
         }
     }
 
-    randomQuote() {
-        this.deleteQuote();
-        this.getQuoteAuthor();
-        this.getQuoteNumber();
-        // console.log(this.getQuoteAuthor());
-
-        if (this.authorValue === "winstonC") {
-            for (let i = 0; i < this.quoteNumber; i++) {
-                this.quoteGenerate = this.generateChurchill.startQuote[Math.floor(Math.random()*this.generateChurchill.startQuote.length)]
-                    +' '+
-                    this.generateChurchill.middleQuote[Math.floor(Math.random()*this.generateChurchill.middleQuote.length)]
-                    +' '+
-                    this.generateChurchill.endQuote[Math.floor(Math.random()*this.generateChurchill.endQuote.length)];
-
-                this.paraQuoteGenerate = document.createElement("p");
-
-                this.paraQuoteGenerate.innerHTML = this.quoteGenerate;
-
-                document.getElementById("containerQuoteGenerate").appendChild(this.paraQuoteGenerate);
-            }
-        } else if (this.authorValue === "albertE") {
-            for (let i = 0; i < this.quoteNumber; i++) {
-                this.quoteGenerate = this.generateEinstein.startQuote[Math.floor(Math.random()*this.generateEinstein.startQuote.length)]
-                    +' '+
-                    this.generateEinstein.middleQuote[Math.floor(Math.random()*this.generateEinstein.middleQuote.length)]
-                    +' '+
-                    this.generateEinstein.endQuote[Math.floor(Math.random()*this.generateEinstein.endQuote.length)];
-
-                this.paraQuoteGenerate = document.createElement("p");
-
-                this.paraQuoteGenerate.innerHTML = this.quoteGenerate;
-
-                document.getElementById("containerQuoteGenerate").appendChild(this.paraQuoteGenerate);
-            }
-        }
-    }
-
+    //Méthode pour supprimer les citations affichées
     deleteQuote() {
         this.containerParaQuoteGenerate = document.getElementById("containerQuoteGenerate");
         this.containerParaQuoteGenerate.innerHTML = "";
     }
 
-
+    // Méthode qui intègre tous les événements
     events() {
-        // console.log(this.generateEinstein.startQuote);
-        // console.log(this.generateChurchill);
-
         document.getElementById("btnGenerate").addEventListener('click', () => {
-            this.randomQuote();
+
+            this.deleteQuote()
+            this.getQuoteAuthor();
+            this.getQuoteNumber();
+
+            if (this.authorValue === "winstonC") {
+                console.log(this.authorValue);
+                churchillQuote.randomQuote(this.generateChurchill, this.quoteNumber);
+            } else if (this.authorValue === "albertE") {
+                einsteinQuote.randomQuote(this.generateEinstein, this.quoteNumber);
+            }
         });
+
         document.getElementById("btnDelete").addEventListener('click', () => {
             this.deleteQuote();
+        });
+
+        // Modifie le style de l'image en fonction du choix de l'auteur
+        let authorName = document.getElementById("quoteAuthorSelect");
+        const einsteinImg = document.getElementById("einsteinImg");
+        const churchillImg = document.getElementById("churchillImg");
+
+        authorName.addEventListener('change', () => {
+
+            switch (authorName.value) {
+                case "empty":
+                    einsteinImg.style.filter = "blur(10px)";
+                    churchillImg.style.filter = "blur(10px)";
+                    break;
+                case "winstonC":
+                    einsteinImg.style.filter = "blur(10px)";
+                    churchillImg.style.filter = "blur(0)";
+                    break;
+                case "albertE":
+                    einsteinImg.style.filter = "blur(0)";
+                    churchillImg.style.filter = "blur(10px)";
+                    break;
+            }
+
         });
     }
 }
